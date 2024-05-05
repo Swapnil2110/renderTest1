ca1fb5289e6d9a426e5502c056a74f247b8968ab
Swapnil Bhagat <swapnil.bhagat@indionetworks.com>
2024-05-06 00:55:19 +0530

    Added a form into this file

--- hello.php
+++ hello.php
@@ -1,10 +1,30 @@
 <?php
-
-$response = array(
-    "message" => "Hello, World!"
-);
-
+// echo print_r($_REQUEST,1);
+$name = $_REQUEST['name'];
+$submit = $_REQUEST['submit'];
 // Encode the response array to JSON and print it
-echo json_encode($response);
+// echo json_encode($response);
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <!-- <input type="text" name="" id=""> -->
+        Enter Your Name : 
+        <input type="text" name="name">
+        <input type="submit" id="submit" name="submit" value="Submit">
+    </form>
+    <div>
+        <p <?= (empty($submit)) ? "hidden" : ""; ?>>Fuck You <?= $name; ?></p>
+    </div>
+</body>
+
+</html>
